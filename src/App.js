2880063faae3ccb7bf462d063974b35a5f58fd6b
John D <jddhalogamer@gmail.com>
2022-05-31 17:53:21 -0500

    added state to input form for function to query database.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { useState, useEffect } from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import axios from 'axios'
-import SearchForm from './SearchForm';
+import Form from 'react-bootstrap/Form'
 import MovieCard from './MovieCard'
+
 
 const App = () => {
 
   const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
 
   const getMovies = async () => {
     const res = await axios({
@@ -20,13 +22,36 @@ const App = () => {
     setMovies(dataArray)
   }
 
+  const searchMovies = async () => {
+    const searchQuery = searchTerm
+    const res = await axios({
+      method: 'GET',
+      url: `/api/movies/search?q=${searchQuery}`
+    })
+  }
+
+  const onChangeSetSearchTerm = (e) => {
+    const setSearch = e.target.value
+    setSearchTerm(setSearch)
+  }
+
   useEffect(() => {
     getMovies()
   }, [])
 
   return (
     <div className="App">
-      <SearchForm />
+      <div className="input-form">
+      <Form className="form">
+			<Form.Group className="mb-3">
+				<Form.Control 
+        placeholder="Input movie title here..."
+        value={searchTerm}
+        onChange={onChangeSetSearchTerm}
+        ></Form.Control>
+			</Form.Group>
+		</Form>
+      </div>
       <div className="container">
       {movies.length > 0 ?
       (movies.map(movie => (
